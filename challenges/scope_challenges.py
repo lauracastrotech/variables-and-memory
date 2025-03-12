924ea7b3500b83080f7ebc34d0fa90b5c072d4af
Laura Castro <lauracastrotech@gmail.com>
2025-03-12 15:48:37 -0400

    complete scope_challenges

--- challenges/scope_challenges.py
+++ challenges/scope_challenges.py
@@ -1,21 +1,23 @@
 def challenge_01():
-    # 1
+    # 1 local
     outer_value = 42
 
     print(f"The value from outside is: {outer_value}")
 
 def challenge_02():
-    # 2
+    # 2 global 
     print(f"The value from outside is: {outer_value}")
 
-def challenge_03():
-    # 3
-    print(f"The value from outside is: {inner_value}")
+# def challenge_03():
+    # 3 Throws error: UnboundLocalError: cannot access local variable 
+    # 'inner_value' where it is not associated with a value
+    # print(f"The value from outside is: {inner_value}")
 
-    inner_value = 42
+    # inner_value = 42
 
 def challenge_04():
     # 4
+    # prints global value of outer_value
     print(f"The value from outside is: {outer_value}")
 
     outer_value = 42
